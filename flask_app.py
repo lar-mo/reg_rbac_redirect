13bb37abb20da9c7c9092dcc1f77ebe5e3facec3
Larry Moiola <lmoiola@gmail.com>
2022-11-05 14:44:15 -0700

    Create flask_app.py

---
+++ flask_app.py
@@ -0,0 +1,16 @@
+from flask import Flask, redirect, request
+
+app = Flask(__name__)
+
+from urllib.parse import urlparse, urlunparse
+
+FROM_DOMAIN = "registration-rbac.com"
+TO_DOMAIN = "www.registration-rbac.com"
+
+@app.before_request
+def redirect_to_new_domain():
+    urlparts = urlparse(request.url)
+    if urlparts.netloc == FROM_DOMAIN:
+        urlparts_list = list(urlparts)
+        urlparts_list[1] = TO_DOMAIN
+        return redirect(urlunparse(urlparts_list), code=301)
